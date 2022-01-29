8ddc218a36de53642076ef034d292c71f213e563
JSheleg <jessica.sheleg@gmail.com>
2022-01-29 00:04:04 -0600

    feat: create Player Class to handle player movement

--- react-rogue/src/ReactRogue.js
+++ react-rogue/src/ReactRogue.js
@@ -1,15 +1,18 @@
 import React, { useRef, useEffect, useState } from 'react';
 import InputManager from './InputManager';
+import Player from './Player';
 
 const ReactRogue =({width, height, tilesize}) => {
     const canvasRef = useRef();
-    const [player, setPlayer] = useState({ x: 64, y: 128 })
+    const [player, setPlayer] = useState(new Player(1,2,tilesize));
     let inputManager = new InputManager();
     const handleInput = (action, data) =>{
         console.log(`handle input: ${action}:${JSON.stringify(data)}`)
-        let newPlayer = {...player };
-        newPlayer.x += data.x * tilesize;
-        newPlayer.y += data.y * tilesize;
+        let newPlayer = new Player();
+        Object.assign(newPlayer, player);
+        newPlayer.move(data.x, data.y);
+        // newPlayer.x += data.x * tilesize;
+        // newPlayer.y += data.y * tilesize;
         setPlayer(newPlayer);
     }
 
@@ -28,8 +31,9 @@ const ReactRogue =({width, height, tilesize}) => {
         const ctx = canvasRef.current.getContext('2d');
         //draw directly to canvas
         ctx.clearRect(0,0, width *tilesize, height*tilesize);
-        ctx.fillStyle='#000';
-        ctx.fillRect(player.x, player.y, 16,16);
+        // ctx.fillStyle='#000';
+        // ctx.fillRect(player.x, player.y, 16,16);
+        player.draw(ctx);
     });
     return(
         <canvas
